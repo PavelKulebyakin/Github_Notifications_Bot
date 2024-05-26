2c97b36115e2d10cd22aed555dd441782fee525d
paul <kulebyakinpaul@yandex.ru>
2024-05-26 15:15:19 +0300

    Add bot commands

---
+++ src/main/java/org/telegram/mybot/util/Command.java
@@ -0,0 +1,9 @@
+package org.telegram.mybot.util;
+
+public class Command {
+
+    public static final String START_COMMAND = "/start";
+    public static final String HELP_COMMAND = "/help";
+    public static final String ADD_COMMAND = "/add";
+
+}
